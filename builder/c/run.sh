be0ef8a1a408a7ae448c986dab6eabf5533f296b
Vink <dong@wenqi.us>
2017-09-05 17:04:25 +0800

    add resilo sync

--- builder/c/run.sh
+++ builder/c/run.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 cd /fluent-bit/build/
+uname -a
 cmake ..
 make
 mv /fluent-bit/build/bin/fluent-bit /result/fluent-bit
-rm -rf /fluent-bit/build/*
+
+/expect_scp.sh 5.5.5.12 root admin /result/fluent-bit /root/build/fluent-bit
